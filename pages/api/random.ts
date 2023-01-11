c01b9bdfec77dbe7287190fdd14b12b93fb12fb9
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2023-01-10 22:55:44 -0600

    Handle no query

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -9,11 +9,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Lyrics>
 ) {
-  const artist = req.query.artist as string;
   let songs: Song[] = [];
 
-  if (artist) songs = await getArtistSongs(artist);
-  else songs = await getChart(new Date());
+  if (req.query) {
+    const artist = req.query.artist as string;
+
+    if (artist) songs = await getArtistSongs(artist);
+  }
+
+  if (songs.length == 0) songs = await getChart(new Date());
 
   let lyrics: Lyrics = { lyrics: "", title: "", artist: "", cover: "" };
   while (lyrics.lyrics.length === 0) {
