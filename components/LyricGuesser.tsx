d325529b135f8b7f7f1e1ee1097951ac4eb0d806
Joe Bouchard <joseph.c.bouchard-1@ou.edu>
2023-01-19 23:10:34 -0600

    Add buttons and animation

--- components/LyricGuesser.tsx
+++ components/LyricGuesser.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer } from "react";
+import { useState, useEffect, useReducer, useRef } from "react";
 import { Lyrics } from "../functions/getLyrics";
 import { Inconsolata } from "@next/font/google";
 import {
@@ -10,20 +10,79 @@ import {
   Button,
   Heading,
   Skeleton,
+  Progress,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+export enum guessTypes {
+  guessed = "guessed",
+  found = "found",
+  invalid = "invalid",
+}
 
 const courier = Inconsolata({ weight: "400" });
 
 const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
   const [guess, setGuess] = useState("");
-  const [guessed, addGuessed] = useReducer(
-    (state: String[], word: string) => [...state, word],
-    [""]
-  );
   const [titleGuess, setTitleGuess] = useState("");
+  const [lastGuess, setLastGuess] = useState<guessTypes>();
 
-  useEffect(() => {
-    console.log(guess);
+  const guessBtn = useRef<HTMLInputElement>(null);
+
+  const [guessed, addGuessed] = useReducer(
+    (
+      state: { found: string[]; invalid: string[] },
+      action: { word: string; valid: boolean }
+    ) => {
+      if (
+        state.found.includes(action.word) ||
+        state.invalid.includes(action.word)
+      )
+        return state;
+      if (action.valid) {
+        return {
+          found: [...state.found, action.word].sort(),
+          invalid: [...state.invalid],
+        };
+      }
+      return {
+        found: [...state.found],
+        invalid: [...state.invalid, action.word].sort(),
+      };
+    },
+    {
+      found: [],
+      invalid: [],
+    }
+  );
+
+  const textVariations = {
+    covered: {
+      color: "var(--chakra-colors-red-700)",
+      backgroundColor: "var(--chakra-colors-red-700)",
+    },
+    revealed: {
+      color: "#000",
+      backgroundColor: "#00000000",
+    },
+  };
+
+  const inputVariations = {
+    guessed: {
+      borderColor: "#333",
+      boxShadow: "#333 0 0 5px",
+    },
+    found: {
+      borderColor: "#070",
+      boxShadow: "#070 0 0 5px",
+    },
+    invalid: {
+      borderColor: "#700",
+      boxShadow: "#700 0 0 5px",
+    },
+  };
+
+  const guessWord = () => {
     const formattedGuess = guess.toLowerCase().replaceAll(/[^A-Za-z 0-9]/g, "");
     const formattedLyrics = lyrics
       .toLowerCase()
@@ -37,16 +96,25 @@ const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
       .replaceAll(/[\n\-]/g, " ")
       .split(" ");
 
-    console.log(formattedLyrics);
     if (
-      !guessed.includes(formattedGuess) &&
-      (formattedLyrics.includes(formattedGuess) ||
-        formattedTitle.includes(formattedGuess))
+      guessed.found.includes(formattedGuess) ||
+      guessed.invalid.includes(formattedGuess)
     ) {
-      addGuessed(formattedGuess);
-      setGuess("");
+      setLastGuess(guessTypes.guessed);
+    } else {
+      const valid =
+        formattedLyrics.includes(formattedGuess) ||
+        formattedTitle.includes(formattedGuess);
+      addGuessed({ word: formattedGuess, valid });
+      setLastGuess(valid ? guessTypes.found : guessTypes.invalid);
     }
-  }, [guess]);
+
+    setGuess("");
+
+    if (guessBtn.current) {
+      guessBtn.current.focus();
+    }
+  };
 
   useEffect(() => {
     if (
@@ -58,14 +126,14 @@ const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
         .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
         .replaceAll(/[\n\-]/g, " ")
         .split(" ")
-        .forEach((l) => addGuessed(l));
+        .forEach((l) => addGuessed({ word: l, valid: true }));
 
       title
         .toLowerCase()
         .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
         .replaceAll(/[\n\-]/g, " ")
         .split(" ")
-        .forEach((l) => addGuessed(l));
+        .forEach((l) => addGuessed({ word: l, valid: true }));
     }
   }, [titleGuess]);
 
@@ -93,40 +161,77 @@ const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
 
   return (
     <Box m={4} bg="white" p={4} borderRadius={4}>
+      <span>
+        {Math.floor(
+          (lyrics
+            .toLowerCase()
+            .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
+            .replaceAll(/[\n\-]/g, " ")
+            .split(" ")
+            .filter((word) => guessed.found.includes(word)).length /
+            lyrics
+              .toLowerCase()
+              .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
+              .replaceAll(/[\n\-]/g, " ")
+              .split(" ").length) *
+            100
+        )}
+        % of words guessed
+      </span>{" "}
+      <Progress
+        mb={2}
+        borderRadius={20}
+        colorScheme="green"
+        value={
+          (lyrics
+            .toLowerCase()
+            .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
+            .replaceAll(/[\n\-]/g, " ")
+            .split(" ")
+            .filter((word) => guessed.found.includes(word)).length /
+            lyrics
+              .toLowerCase()
+              .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
+              .replaceAll(/[\n\-]/g, " ")
+              .split(" ").length) *
+          100
+        }
+      />
       <HStack spacing={8}>
         <Heading>
           {title.split(" ").map((t, k) => (
             <>
-              <span
+              <motion.span
                 key={`${t} ${k}`}
-                style={{
-                  color: guessed.includes(t.toLowerCase().replaceAll(/\W/g, ""))
-                    ? "#070"
-                    : "#700",
-                  backgroundColor: guessed.includes(
-                    t.toLowerCase().replaceAll(/\W/g, "")
-                  )
-                    ? "#00000000"
-                    : "#700",
-                }}
+                initial={textVariations.covered}
+                variants={textVariations}
+                animate={
+                  guessed.found.includes(t.toLowerCase().replaceAll(/\W/g, ""))
+                    ? "revealed"
+                    : "covered"
+                }
               >
-                {t}
-              </span>{" "}
+                {guessed.found.includes(t.toLowerCase().replaceAll(/\W/g, ""))
+                  ? t
+                  : t.replaceAll(/\w/g, "X")}
+              </motion.span>{" "}
             </>
           ))}
         </Heading>
+
         <Button
           onClick={() => {
             const formattedLyrics = lyrics
               .toLowerCase()
               .replaceAll(/[^A-Za-z \n\-0-9]/g, "")
               .replaceAll(/[\n\-]/g, " ")
-              .split(" ");
+              .split(" ")
+              .filter((word) => !guessed.found.includes(word));
 
             let choice = "";
             let counter = 0;
 
-            while (guessed.includes(choice) && counter < 10) {
+            while (guessed.found.includes(choice) && counter < 20) {
               choice =
                 formattedLyrics[
                   Math.floor(Math.random() * formattedLyrics.length)
@@ -134,23 +239,44 @@ const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
               counter += 1;
             }
 
-            setGuess(choice);
+            addGuessed({ word: choice, valid: true });
+            guessBtn.current?.focus();
           }}
-          colorScheme="blue"
+          colorScheme="purple"
         >
           Hint?
         </Button>
+        <Button
+          onClick={() => {
+            setTitleGuess(title);
+          }}
+          colorScheme="red"
+        >
+          Give Up?
+        </Button>
       </HStack>
-      <HStack spacing={24}>
+      <HStack mb={2} spacing={24}>
         <Stack>
           <Text>Guess a word</Text>
-          <Input
-            bg="white"
-            value={guess}
-            onChange={(e) => setGuess(e.target.value)}
-            borderColor="blue.700"
-            shadow="md"
-          />
+          <HStack>
+            <Input
+              bg="white"
+              _focus={lastGuess && inputVariations[lastGuess]}
+              ref={guessBtn}
+              value={guess}
+              onChange={(e) => setGuess(e.target.value)}
+              boxShadow="none"
+              borderWidth="2px"
+              onKeyDown={(e) => {
+                if (e.code === "Enter") {
+                  guessWord();
+                }
+              }}
+            />
+            <Button colorScheme="green" onClick={guessWord}>
+              Guess
+            </Button>
+          </HStack>
         </Stack>
         <Stack>
           <Text>Guess the title</Text>
@@ -165,7 +291,7 @@ const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
       </HStack>
       <div
         style={{
-          height: "75vh",
+          height: "70vh",
           overflowX: "scroll",
           display: "flex",
           flexDirection: "column",
@@ -183,30 +309,55 @@ const LyricGuesser: React.FC<Lyrics> = ({ title, lyrics }) => {
           >
             {l.split(/[ \-]/).map((w, wk) => (
               <>
-                <Text
+                <motion.span
                   className={courier.className}
-                  display="inline-block"
-                  color={
-                    guessed.includes(w.toLowerCase().replaceAll(/\W/g, ""))
-                      ? "black"
-                      : "red.700"
+                  initial={textVariations.covered}
+                  variants={textVariations}
+                  animate={
+                    guessed.found.includes(
+                      w.toLowerCase().replaceAll(/\W/g, "")
+                    )
+                      ? "revealed"
+                      : "covered"
                   }
-                  backgroundColor={
-                    guessed.includes(w.toLowerCase().replaceAll(/\W/g, ""))
-                      ? "none"
-                      : "red.700"
-                  }
-                  key={`${k} ${wk}`}
                 >
-                  {guessed.includes(w.toLowerCase().replaceAll(/\W/g, ""))
+                  {guessed.found.includes(w.toLowerCase().replaceAll(/\W/g, ""))
                     ? w
                     : w.replaceAll(/\w/g, "X")}
-                </Text>{" "}
+                </motion.span>{" "}
               </>
             ))}
           </div>
         ))}
       </div>
+      <Box p={4} mt={2} border="1px solid #222" borderRadius={12}>
+        <Heading size="sm">Guessed Words:</Heading>
+        <hr
+          style={{
+            color: "#000",
+            border: "0.5px solid #999",
+          }}
+        />
+        <Text my={2} size="lg">
+          Valid Words:
+        </Text>
+        {guessed.found.map((w) => (
+          <span key={w}>{w} </span>
+        ))}
+        <hr
+          style={{
+            color: "#000",
+            border: "0.5px solid #999",
+            margin: "16px 0px",
+          }}
+        />
+        <Text my={2} size="lg">
+          Invalid Words:
+        </Text>
+        {guessed.invalid.map((w) => (
+          <span key={w}>{w} </span>
+        ))}
+      </Box>
     </Box>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../chakra/theme";
+import { AnimatePresence } from "framer-motion";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait" initial={false}>
+        <Component {...pageProps} />
+      </AnimatePresence>
     </ChakraProvider>
   );
 };
